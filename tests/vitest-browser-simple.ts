6ec13411f6ccf9b3d797dd1000761d99edcdaa59
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-09-22 17:37:13 +0200

    chore: update playwright command

--- tests/vitest-browser-simple.ts
+++ tests/vitest-browser-simple.ts
@@ -6,6 +6,6 @@ export async function test(options: RunOptions) {
 		...options,
 		repo: 'vitest-tests/browser-simple',
 		test: 'test',
-		beforeTest: ['pnpm playwright-core install firefox'],
+		beforeTest: ['pnpm playwright install firefox'],
 	})
 }
