545ad8f17f8906081c130a45e12501830f0c3389
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-04 12:29:06 +0900

    chore: retry

--- tests/vitest-vscode.ts
+++ tests/vitest-vscode.ts
@@ -13,7 +13,7 @@ export async function test(options: RunOptions) {
 		test: async () => {
 			if (process.env.CI === 'true' && process.platform === 'linux') {
 				await $`xvfb-run --auto-servernum --server-args=-screen\\ 0\\ 1024x768x24 pnpm test`
-				await $`xvfb-run --auto-servernum --server-args=-screen\\ 0\\ 1024x768x24 pnpm test-e2e`
+				await $`xvfb-run --auto-servernum --server-args=-screen\\ 0\\ 1024x768x24 pnpm test-e2e --retry 2`
 			} else {
 				await $`pnpm test`
 				await $`pnpm test-e2e`
