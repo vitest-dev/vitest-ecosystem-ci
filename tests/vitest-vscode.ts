3651e1ce4218d72f4a27a22ec2f041920a6973c0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-04 09:06:06 +0900

    ci: use xvfb-run

--- tests/vitest-vscode.ts
+++ tests/vitest-vscode.ts
@@ -1,14 +1,19 @@
-import { runInRepo } from '../utils'
+import { runInRepo, $ } from '../utils'
 import { RunOptions } from '../types'
-
-// npx tsx ecosystem-ci.ts vitest-vscode --release 1.3.1
+import process from 'node:process'
 
 export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		build: 'compile',
 		repo: 'vitest-dev/vscode',
-		test: 'test',
+		test: async () => {
+			if (process.env.CI === 'true' && process.platform === 'linux') {
+				await $`xvfb-run --auto-servernum --server-args='-screen 0 1024x768x24' pnpm test`
+			} else {
+				await $`pnpm test`
+			}
+		},
 
 		// https://github.com/vitest-dev/vscode/pull/276
 		// repo: 'hi-ogawa/vitest-vscode',
