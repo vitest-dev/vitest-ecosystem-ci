73000c4ae2c9ca65ca45cce450c247b0f561cc77
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-04 12:06:47 +0900

    chore: add test-e2e

--- tests/vitest-vscode.ts
+++ tests/vitest-vscode.ts
@@ -6,18 +6,20 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		build: 'compile',
-		repo: 'vitest-dev/vscode',
+		// repo: 'vitest-dev/vscode',
+
+		// https://github.com/vitest-dev/vscode/pull/276
+		repo: 'hi-ogawa/vitest-vscode',
+		branch: 'test-vscode-e2e',
+
 		test: async () => {
 			if (process.env.CI === 'true' && process.platform === 'linux') {
 				await $`xvfb-run --auto-servernum --server-args=-screen\\ 0\\ 1920x1080x24 pnpm test`
+				await $`xvfb-run --auto-servernum --server-args=-screen\\ 0\\ 1920x1080x24 pnpm test-e2e`
 			} else {
 				await $`pnpm test`
+				await $`pnpm test-e2e`
 			}
 		},
-
-		// https://github.com/vitest-dev/vscode/pull/276
-		// repo: 'hi-ogawa/vitest-vscode',
-		// branch: 'test-vscode-e2e',
-		// test: ['test', 'test-e2e'],
 	})
 }
