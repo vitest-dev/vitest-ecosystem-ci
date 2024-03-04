f6afa6056343f0b3b60cb5861815fc8af8aac18a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-04 12:15:48 +0900

    chore: tweak

--- tests/vitest-vscode.ts
+++ tests/vitest-vscode.ts
@@ -5,13 +5,11 @@ import process from 'node:process'
 export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
-		build: 'compile',
 		// repo: 'vitest-dev/vscode',
-
 		// https://github.com/vitest-dev/vscode/pull/276
 		repo: 'hi-ogawa/vitest-vscode',
 		branch: 'test-vscode-e2e',
-
+		build: 'compile',
 		test: async () => {
 			if (process.env.CI === 'true' && process.platform === 'linux') {
 				await $`xvfb-run --auto-servernum --server-args=-screen\\ 0\\ 1024x768x24 pnpm test`
