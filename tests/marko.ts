950f8cc0604763023cf59c2b1129e84193254ae9
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-05-14 11:37:36 +0200

    chore: update dir

--- tests/marko.ts
+++ tests/marko.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'marko-js/examples',
-		dir: 'library-ts',
+		dir: 'examples/library-ts',
 		test: 'test',
 		branch: 'master',
 	})
