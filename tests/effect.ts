7ec724c74e735ef03fb64af7fa3bdc6dde9b6ceb
Ari Perkkiö <ari.perkkio@gmail.com>
2024-07-08 08:51:00 +0300

    fix(effect): shard tests to prevent timout

--- tests/effect.ts
+++ tests/effect.ts
@@ -5,6 +5,6 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'Effect-TS/effect',
-		test: 'test',
+		test: ['test --shard 1/3', 'test --shard 2/3', 'test --shard 3/3'],
 	})
 }
