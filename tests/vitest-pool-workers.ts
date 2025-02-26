4d0286b210e4689d541caeaf4252ff52ef3fcb24
Edmund Hung <me@edmund.dev>
2025-02-26 05:48:48 +0000

    feat: add `vitest-pool-workers` (#30)

---
+++ tests/vitest-pool-workers.ts
@@ -0,0 +1,10 @@
+import { runInRepo } from '../utils'
+import { RunOptions } from '../types'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'cloudflare/workers-sdk',
+		test: 'pnpm test:ci -F @cloudflare/vitest-pool-workers-examples',
+	})
+}
