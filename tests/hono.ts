0d273c864568f6169552721872fb9a0f26330158
Jonathan Haines <jonno.haines@gmail.com>
2025-06-27 00:49:59 +1000

    feature: add honojs/middleware

---
+++ tests/hono.ts
@@ -0,0 +1,10 @@
+import { runInRepo } from '../utils'
+import { RunOptions } from '../types'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'honojs/middleware',
+		test: 'test',
+	})
+}
