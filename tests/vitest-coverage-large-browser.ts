17010ac72b5db8aa589b87a62f92fbcd4bc6f1bc
Ari Perkkiö <ari.perkkio@gmail.com>
2026-07-21 10:42:55 +0300

    feat: add `vitest-coverage-large` browser project

--- tests/vitest-coverage-large-browser.ts
+++ tests/vitest-coverage-large-browser.ts
@@ -5,6 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'vitest-tests/coverage-large',
-		test: 'test',
+		test: 'test:browser',
+		beforeTest: ['pnpm playwright install chromium'],
 	})
 }
